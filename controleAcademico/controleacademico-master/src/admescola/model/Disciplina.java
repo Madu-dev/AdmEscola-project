b02df2266414018d9c6a603efbf469523f0bf2eb
Maria do Carmo <69973218+Madu-dev@users.noreply.github.com>
2022-04-30 21:12:42 -0300

    Add files via upload

---
+++ controleAcademico/controleacademico-master/src/admescola/model/Disciplina.java
@@ -0,0 +1,92 @@
+package admescola.model;
+
+import java.io.Serializable;
+import javafx.scene.control.Button;
+
+
+public class Disciplina implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    private final String codigo;
+    private String nome;
+    private String cargaHoraria;
+    private String creditos;
+    private String periodo;
+    private boolean alocada;
+    private String registrada;
+   
+
+    public Disciplina(String codigo, String nome, String cargaHoraria, String creditos, String periodo) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargaHoraria = cargaHoraria;
+        this.creditos = creditos;
+        this.periodo = periodo;
+        this.alocada = false;
+        this.registrada = "Não";
+        
+        
+    }
+
+    public String getCodigo(){
+        return codigo;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(String cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(String creditos) {
+        this.creditos = creditos;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+
+    public boolean isAlocada() {
+        return alocada;
+    }
+
+    public void setAlocada(boolean alocada) {
+        this.alocada = alocada;
+        if(this.alocada==true){
+            this.registrada = "Sim";
+        }else{
+            this.registrada = "Não";
+        }
+    }
+
+    public String getRegistrada() {
+        return registrada;
+    }
+
+    public void setRegistrada(String registrada) {
+        this.registrada = registrada;
+    }
+
+   
+   
+
+}
---
+++ controleAcademico/controleacademico-master/src/admescola/model/UsuarioTurma.java
@@ -0,0 +1,39 @@
+package admescola.model;
+
+public class UsuarioTurma {
+    private Usuario aluno;
+    private Disciplina disciplina;
+    private Turma turma;
+
+    public UsuarioTurma(Usuario aluno, Disciplina disciplina, Turma turma) {
+        this.aluno = aluno;
+        this.disciplina = disciplina;
+        this.turma = turma;
+    }
+
+    public Usuario getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(Usuario aluno) {
+        this.aluno = aluno;
+    }
+
+    public Disciplina getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(Disciplina disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public Turma getTurma() {
+        return turma;
+    }
+
+    public void setTurma(Turma turma) {
+        this.turma = turma;
+    }
+    
+    
+}
